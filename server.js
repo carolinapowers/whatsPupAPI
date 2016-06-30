6dbcb30dba78690a80e46baea28f4b9c8dad63d8
cpowers <cpowers@deloitte.com>
2016-06-30 09:57:06 -0400

    added package.json and server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var app = express(); 
+var PORT = process.env.PORT || 8080; 
+var cors = require ('cors');
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+require('./app/routing/api-routes.js')(app); 
+require('./app/routing/html-routes.js')(app);
