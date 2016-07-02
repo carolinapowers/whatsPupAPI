70c339e747f8f0952b4fcc141f3581c64bf9fa7a
Carolina Powers <carolinapoloni@gmail.com>
2016-07-01 23:53:48 -0400

    changind body parser url encoded to false

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || 8080;
 var cors = require ('cors');
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
